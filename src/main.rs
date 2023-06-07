e372319b8469c592fe8ad7c8aa41a5e73751b981
Eva <eva@evatw.in>
2023-06-07 12:29:47 -0230

    first commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use std::process::{Command};
+use std::env::args;
+use serde_json::{Value};
+use std::fs::read_to_string;
+
+fn main() {
+    let args: Vec<String> = args().collect();
+
+    let configuration_json: Value = serde_json::from_str(
+    read_to_string("./config.json")
+        .expect("Could not read the configuration json.")
+        .as_str()
+    ).unwrap();
+
+    let mut arguments_string = "".to_string();
+    let arguments: Vec<(String, Value)> = configuration_json["args"].as_object().unwrap().iter().map(|x| (x.0.to_owned(), x.1.to_owned())).collect();
+
+    for i in arguments {
+        arguments_string.push_str(format!("{}={}:", i.0, i.1).as_str());
+    }
+
+    arguments_string = arguments_string
+        .replace("\\", "")
+        .replace("\"", "")
+        .strip_suffix(":").unwrap().to_string();
+
+    let filename_arg = match args.len() {
+        1 => args[0].as_str(),
+        _ => args[1].as_str()
+    };
+
+    let _command = Command::new("./ffmpeg")
+        .args([
+            "-loop", "1",
+            "-i", "./thumbnail.png",
+            "-i", filename_arg,
+            "-map", "0",
+            "-map", "1:a",
+            "-vf", format!("fps=30,format=yuv420p,fade={}", arguments_string.as_str()).as_str(),
+            "-c:v", "libx264",
+            "-preset", "ultrafast",
+            "-tune", "stillimage",
+            "-framerate", "1",
+            "-c:a", "aac",
+            "-b:a", "320k",
+            "-shortest",
+            "-y",
+            "output.mp4"
+        ])
+        .spawn();
+    // let process = Command::new("ffmpeg")
+    //     .stdin(Stdio::piped())
+    //     .spawn();
+}
